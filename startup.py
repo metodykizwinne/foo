ce8687df6970b0d2632bee86d39c1b2ad3201630
Filip Pawlak <filip.pawlak@gmail.com>
2012-11-06 21:24:07 +0100

    Mariusz

---
+++ startup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+     
+from Tkinter import *
+ 
+ 
+class StartUpWindow:
+ 
+    def __init__(self, master, numbers):
+ 
+        frame = Frame(master)
+        frame.pack()
+ 
+        for e in numbers:
+            self.makeButton(e,frame)
+ 
+    def makeButton(self,number,frame):
+        self.number = Button(frame, text="Sprawa nr " + str(number), fg="red",
+        command = lambda: self.open_investiagtion(number))
+        self.number.pack()
+ 
+    def open_investiagtion(self,e):
+        print 'Otworz okno sprawy %s.' % str(e)
+ 
+ 
+# root = Tk()
+# app = StartUpWindow(root, range(10))
+# root.mainloop()
