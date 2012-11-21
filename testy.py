db615c7dea643011a156920a25ccf9a021eb69be
Filip Pawlak <filip.pawlak@gmail.com>
2012-11-21 20:00:20 +0100

    nowe testy

--- testy.py
+++ testy.py
@@ -9,31 +9,46 @@ class TestInvestigationEditing(unittest.TestCase):
     def setUp(self):
         self.conn = psycopg2.connect("port=5432 host=metodyki.dyndns.org dbname=test user=pguser password='tylkosystemlinux'")
         self.cur = conn.cursor()
-        self.cur.execute("CREATE TABLE uprawnienia ( Policjant varchar(255), Sprawa varchar(255), Uprawnienia varchar(255) );" + \
-        "INSERT INTO uprawnienia VALUES ('P100','S100','odczyt');" + \
-        "INSERT INTO uprawnienia VALUES ('P101','S100','odczyt');" + \
-        "INSERT INTO uprawnienia VALUES ('P102','S100','odczyt/zapis');" + \
-        "INSERT INTO uprawnienia VALUES ('P103','S100','odczyt');" + \
-        "INSERT INTO uprawnienia VALUES ('P104','S100','odczyt');" + \
-        "INSERT INTO uprawnienia VALUES ('P100','S101','odczyt');" + \
-        "INSERT INTO uprawnienia VALUES ('P101','S101','odczyt/zapis');" + \
-        "INSERT INTO uprawnienia VALUES ('P102','S100','odczyt');" + \
-        "INSERT INTO uprawnienia VALUES ('P105','S100','odczyt');" + \
-        "INSERT INTO uprawnienia VALUES ('P100','S102','odczyt/zapis')")
+        self.cur.execute("CREATE TABLE uprawnienia ( Sprawa varchar(255), Policjant varchar(255), Uprawnienia varchar(255) );" + \
+        "INSERT INTO uprawnienia VALUES ('S100','P100','odczyt');" + \
+        "INSERT INTO uprawnienia VALUES ('S100','P101','odczyt');" + \
+        "INSERT INTO uprawnienia VALUES ('S100','P102','odczyt/zapis');" + \
+        "INSERT INTO uprawnienia VALUES ('S100','P103','odczyt');" + \
+        "INSERT INTO uprawnienia VALUES ('S100','P104','odczyt');" + \
+        "INSERT INTO uprawnienia VALUES ('S100','P105','odczyt');" + \
+        "INSERT INTO uprawnienia VALUES ('S101','P100','odczyt');" + \
+        "INSERT INTO uprawnienia VALUES ('S101','P101','odczyt/zapis');" + \
+        "INSERT INTO uprawnienia VALUES ('S102','P100','odczyt/zapis')")
 
     def test_set_privileges(self):
+        # opcjonalnie można potem sprawdzać całą tabelę (albo przynajmniej czy liczba rekordów się zgadza)
+        
+        # brak sprawy
         with self.assertRaises(NoSuchCaseError):
             core.set_privileges('S105','P100','odczyt')
 
+        # brak użytkownika
         with self.assertRaises(NoSuchUserError):
             core.set_privileges('S100', 'P106', 'odczyt/zapis')
-# powinna być jeszcze przynajmniej zmiana uprawnień
-# + opcjonalnie można potem sprawdzać całą tabelę albo przynajmniej czy liczba rekordów się zgadza
 
+        # usunięcie uprawnień
         core.set_privileges('S100', 'P102', None)
-        cur.execute("SELECT * FROM uprawnienia WHERE Sprawa='S100' AND Policjant='P102'")
+        cur.execute("SELECT NULL FROM uprawnienia WHERE Sprawa='S100' AND Policjant='P102'")
         self.assertEqual(cur.rowcount, 0)
 
+        # zmiana uprawnień
+        core.set_privileges('S101', 'P100', 'odczyt/zapis')
+        cur.execute("SELECT Uprawnienia FROM uprawnienia WHERE Sprawa='S101' AND Policjant='P100'")
+        self.assertEqual(cur.rowcount, 1)
+        (privileges,) = cur.fetchone()
+        self.assertEqual(privileges, 'odczyt/zapis')
+
+        # dodanie uprawnień do sprawy nowemu użytkownikowi
+        core.set_privileges('S101', 'P105', 'odczyt')
+        cur.execute("SELECT Uprawnienia FROM uprawnienia WHERE Sprawa='S101' AND Policjant='P105'")
+        self.assertEqual(cur.rowcount, 1)
+        (privileges,) = cur.fetchone()
+        self.assertEqual(privileges, 'odczyt')
 
     def tearDown(self):
         self.cur.execute("DROP TABLE uprawnienia")
