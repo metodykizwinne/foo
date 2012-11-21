91aff3ee10ca0f133c22a1183e80c28c9c2c57f3
Filip Pawlak <filip.pawlak@gmail.com>
2012-11-21 19:23:57 +0100

    drobnostki

---
+++ core.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+def set_privileges(user, investigation, privileges):
+    return
--- testy.py
+++ testy.py
@@ -7,19 +7,19 @@ import core
 class TestInvestigationEditing(unittest.TestCase):
 
     def setUp(self):
-        self.conn = psycopg2.connect("port=5432 host=metodyki.dynds.org dbname=test user=pguser password='tylkosystemlinux'")
+        self.conn = psycopg2.connect("port=5432 host=metodyki.dyndns.org dbname=test user=pguser password='tylkosystemlinux'")
         self.cur = conn.cursor()
-        self.cur.execute("CREATE TABLE uprawnienia ( U_Id int, Policjant varchar(255), Sprawa varchar(255), Uprawnienia varchar(255) );" + \
-        "INSERT INTO uprawnienia ('P100','S100','odczyt');" + \
-        "INSERT INTO uprawnienia ('P101','S100','odczyt');" + \
-        "INSERT INTO uprawnienia ('P102','S100','odczyt/zapis');" + \
-        "INSERT INTO uprawnienia ('P103','S100','odczyt');" + \
-        "INSERT INTO uprawnienia ('P104','S100','odczyt');" + \
-        "INSERT INTO uprawnienia ('P100','S101','odczyt');" + \
-        "INSERT INTO uprawnienia ('P101','S101','odczyt/zapis');" + \
-        "INSERT INTO uprawnienia ('P102','S100','odczyt');" + \
-        "INSERT INTO uprawnienia ('P105','S100','odczyt');" + \
-        "INSERT INTO uprawnienia ('P100','S102','odczyt/zapis');")
+        self.cur.execute("CREATE TABLE uprawnienia ( Policjant varchar(255), Sprawa varchar(255), Uprawnienia varchar(255) );" + \
+        "INSERT INTO uprawnienia VALUES ('P100','S100','odczyt');" + \
+        "INSERT INTO uprawnienia VALUES ('P101','S100','odczyt');" + \
+        "INSERT INTO uprawnienia VALUES ('P102','S100','odczyt/zapis');" + \
+        "INSERT INTO uprawnienia VALUES ('P103','S100','odczyt');" + \
+        "INSERT INTO uprawnienia VALUES ('P104','S100','odczyt');" + \
+        "INSERT INTO uprawnienia VALUES ('P100','S101','odczyt');" + \
+        "INSERT INTO uprawnienia VALUES ('P101','S101','odczyt/zapis');" + \
+        "INSERT INTO uprawnienia VALUES ('P102','S100','odczyt');" + \
+        "INSERT INTO uprawnienia VALUES ('P105','S100','odczyt');" + \
+        "INSERT INTO uprawnienia VALUES ('P100','S102','odczyt/zapis')")
 
     def test_set_privileges(self):
         with self.assertRaises(NoSuchCaseError):
@@ -30,9 +30,11 @@ class TestInvestigationEditing(unittest.TestCase):
 # powinna być jeszcze przynajmniej zmiana uprawnień i usunięcie rekordu poprzez ich odebranie
 # + opcjonalnie można potem sprawdzać całą tabelę albo przynajmniej czy liczba rekordów się zgadza
 
+        core.set_privileges('S100', 'P102', None)
+
 
     def tearDown(self):
-        self.cur.execute("DROP TABLE uprawnienia;")
+        self.cur.execute("DROP TABLE uprawnienia")
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestSequenceFunctions)
 unittest.TextTestRunner(verbosity=2).run(suite)
