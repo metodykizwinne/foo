abec00b1af3e6e3e61ebae76186abf7a6e86e042
Filip Pawlak <filip.pawlak@gmail.com>
2012-11-21 20:45:43 +0100

    to działa

--- core.py
+++ core.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
 
+class NoSuchUserError(Exception): pass
+class NoSuchCaseError(Exception): pass
+
 def set_privileges(user, investigation, privileges):
     return
--- testy.py
+++ testy.py
@@ -4,65 +4,60 @@ import psycopg2
 import unittest
 import core
 
-class InvestigationEditingTC(unittest.TestCase):
+class InvestigationEditingTestCase(unittest.TestCase):
 
     def setUp(self):
         self.conn = psycopg2.connect("port=5432 host=metodyki.dyndns.org dbname=test user=pguser password='tylkosystemlinux'")
-        self.cur = conn.cursor()
+        self.cur = self.conn.cursor()
         self.cur.execute("CREATE TABLE uprawnienia ( id serial PRIMARY KEY, Sprawa varchar(255), Policjant varchar(255), Uprawnienia varchar(255) );" + \
-        "INSERT INTO uprawnienia VALUES ('S100','P100','odczyt');" + \
-        "INSERT INTO uprawnienia VALUES ('S100','P101','odczyt');" + \
-        "INSERT INTO uprawnienia VALUES ('S100','P102','odczyt/zapis');" + \
-        "INSERT INTO uprawnienia VALUES ('S100','P103','odczyt');" + \
-        "INSERT INTO uprawnienia VALUES ('S100','P104','odczyt');" + \
-        "INSERT INTO uprawnienia VALUES ('S100','P105','odczyt');" + \
-        "INSERT INTO uprawnienia VALUES ('S101','P100','odczyt');" + \
-        "INSERT INTO uprawnienia VALUES ('S101','P101','odczyt/zapis');" + \
-        "INSERT INTO uprawnienia VALUES ('S102','P100','odczyt/zapis')")
+        "INSERT INTO uprawnienia (Sprawa, Policjant, Uprawnienia) VALUES ('S100','P100','odczyt');" + \
+        "INSERT INTO uprawnienia (Sprawa, Policjant, Uprawnienia) VALUES ('S100','P101','odczyt');" + \
+        "INSERT INTO uprawnienia (Sprawa, Policjant, Uprawnienia) VALUES ('S100','P102','odczyt/zapis');" + \
+        "INSERT INTO uprawnienia (Sprawa, Policjant, Uprawnienia) VALUES ('S100','P103','odczyt');" + \
+        "INSERT INTO uprawnienia (Sprawa, Policjant, Uprawnienia) VALUES ('S100','P104','odczyt');" + \
+        "INSERT INTO uprawnienia (Sprawa, Policjant, Uprawnienia) VALUES ('S100','P105','odczyt');" + \
+        "INSERT INTO uprawnienia (Sprawa, Policjant, Uprawnienia) VALUES ('S101','P100','odczyt');" + \
+        "INSERT INTO uprawnienia (Sprawa, Policjant, Uprawnienia) VALUES ('S101','P101','odczyt/zapis');" + \
+        "INSERT INTO uprawnienia (Sprawa, Policjant, Uprawnienia) VALUES ('S102','P100','odczyt/zapis')")
 
     def tearDown(self):
         self.cur.execute("DROP TABLE uprawnienia")
+        self.cur.close()
+        self.conn.close()
 
-# opcjonalnie można potem sprawdzać całą tabelę (albo przynajmniej czy liczba rekordów się zgadza)
-        
-class NoSuchCaseTC(InvestigationEditingTC):
+    # opcjonalnie można potem sprawdzać całą tabelę (albo przynajmniej czy liczba rekordów się zgadza)
+
     # brak sprawy
-    def runTest(self):
-        with self.assertRaises(NoSuchCaseError):
+    def test_NoSuchCase(self):
+        with self.assertRaises(core.NoSuchCaseError):
             core.set_privileges('S105','P100','odczyt')
 
-class NoSuchUserTC(InvestigationEditingTC):
     # brak użytkownika
-    def runTest(self):
-        with self.assertRaises(NoSuchUserError):
+    def test_NoSuchUser(self):
+        with self.assertRaises(core.NoSuchUserError):
             core.set_privileges('S100', 'P106', 'odczyt/zapis')
 
-class TakeAwayPrivilegesTC(InvestigationEditingTC):            
     # usunięcie uprawnień
-    def runTest(self):
+    def test_TakeAwayPrivileges(self):
         core.set_privileges('S100', 'P102', None)
-        cur.execute("SELECT NULL FROM uprawnienia WHERE Sprawa='S100' AND Policjant='P102'")
-        self.assertEqual(cur.rowcount, 0)
+        self.cur.execute("SELECT NULL FROM uprawnienia WHERE Sprawa='S100' AND Policjant='P102'")
+        self.assertEqual(self.cur.rowcount, 0)
 
-class ChangePrivilegesTC(InvestigationEditingTC):
     # zmiana uprawnień
-    def runTest(self):
+    def test_ChangePrivileges(self):
         core.set_privileges('S101', 'P100', 'odczyt/zapis')
-        cur.execute("SELECT Uprawnienia FROM uprawnienia WHERE Sprawa='S101' AND Policjant='P100'")
-        self.assertEqual(cur.rowcount, 1)
-        (privileges,) = cur.fetchone()
+        self.cur.execute("SELECT Uprawnienia FROM uprawnienia WHERE Sprawa='S101' AND Policjant='P100'")
+        self.assertEqual(self.cur.rowcount, 1)
+        (privileges,) = self.cur.fetchone()
         self.assertEqual(privileges, 'odczyt/zapis')
 
-class GivePrivilegesTC(InvestigationEditingTC):        
     # dodanie uprawnień do sprawy nowemu użytkownikowi
-    def runTest(self):
+    def test_GivePrivileges(self):
         core.set_privileges('S101', 'P105', 'odczyt')
-        cur.execute("SELECT Uprawnienia FROM uprawnienia WHERE Sprawa='S101' AND Policjant='P105'")
-        self.assertEqual(cur.rowcount, 1)
-        (privileges,) = cur.fetchone()
+        self.cur.execute("SELECT Uprawnienia FROM uprawnienia WHERE Sprawa='S101' AND Policjant='P105'")
+        self.assertEqual(self.cur.rowcount, 1)
+        (privileges,) = self.cur.fetchone()
         self.assertEqual(privileges, 'odczyt')
 
-
-
-# suite = unittest.TestLoader().loadTestsFromTestCase(TestSequenceFunctions)
-# unittest.TextTestRunner(verbosity=2).run(suite)
+suite = unittest.TestLoader().loadTestsFromTestCase(InvestigationEditingTestCase)
+unittest.TextTestRunner(verbosity=2).run(suite)
